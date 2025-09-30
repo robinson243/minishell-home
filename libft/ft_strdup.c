24694df58719de03f2d41fad25232bfcc7d62aa1
Robinson Mukena Zevo <romukena@f1br7s8.clusters.42paris.fr>
2025-09-30 10:49:37 +0200

    add libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 00:48:32 by romukena          #+#    #+#             */
+/*   Updated: 2025/04/30 12:36:03 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	dest = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!dest)
+		return (NULL);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/* int main(void)
+{
+	char *src = "hello";
+	char *dest;
+	dest = ft_strdup(src);
+
+
+	printf("%s", dest);
+} */
