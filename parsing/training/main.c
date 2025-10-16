1303c3265e76d3f32d1e409668411f8b35f8a1d7
Robinson Mukena Zevo <romukena@f6r13s8.clusters.42paris.fr>
2025-10-16 15:26:53 +0200

    finish management quote

--- parsing/training/main.c
+++ parsing/training/main.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:01:15 by romukena          #+#    #+#             */
-/*   Updated: 2025/10/16 14:31:37 by romukena         ###   ########.fr       */
+/*   Updated: 2025/10/16 14:54:04 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -273,14 +273,58 @@ t_node	*lexer(char *input, t_node **head)
 
 int	main(void)
 {
-	t_node	*head;
-	char	*tests;
 
-	head = NULL;
-	tests = "echo he\"llo\"yap\" youpi\"";
-	lexer(tests, &head);
-	print_list(&head);
-	clear_nodes(&head);
-	printf("\n");
+	t_node	*head;
+	char	*tests[] = {
+		"echo salut",
+		"ls",
+		"pwd",
+		"exit",
+		"echo      salut      les    gens",
+		"ls        -l",
+		"echo \"salut les gens\"",
+		"echo 'salut les gens'",
+		"echo \"bonjour 'tout' le monde\"",
+		"echo 'bonjour \"tout\" le monde'",
+		"echo \"a'b'c\"",
+		"echo 'a\"b\"c'",
+		"echo salut > out.txt",
+		"cat < in.txt",
+		"cat < in.txt > out.txt",
+		"echo salut >> out.txt",
+		"echo salut > out.txt > out2.txt",
+		"ls | wc -l",
+		"ls -l | grep minishell",
+		"cat in.txt | grep salut | wc -l",
+		"echo \"salut | les gens\"",
+		"echo salut | echo bonjour",
+		"echo \"salut\" > out.txt | cat -e",
+		"cat << EOF",
+		"echo \"salut\" 'les' \"gens\" > file.txt | cat file.txt",
+		"|",
+		"||",
+		">",
+		">>",
+		"<",
+		"<<",
+		"| echo salut",
+		"echo salut |",
+		"echo \"salut",
+		"echo 'salut",
+		NULL
+	};
+	int	i = 0;
+
+	while (tests[i])
+	{
+		head = NULL;
+		printf("=== TEST %d ===\n", i + 1);
+		printf("Input: [%s]\n", tests[i]);
+		lexer(tests[i], &head);
+		print_list(&head);
+		clear_nodes(&head);
+		printf("\n");
+		i++;
+	}
 	return (0);
 }
