a5da1fa8e70cbb829b2906f5cda81c2fe5029496
Robinson Mukena Zevo <romukena@f2r11s20.clusters.42paris.fr>
2025-11-01 16:41:59 +0100

    add some functions

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:41:37 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/01 10:26:00 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/01 16:35:15 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,42 @@ int	numb_of_commands(t_node **head)
 		i++;
 	}
 	return (i);
+}
+
+t_cmd *new_cmd(void)
+{
+	t_cmd	*node;
+
+	node = malloc(sizeof(t_cmd));
+	if (!node)
+		return (NULL);
+	return (node);
+}
+
+t_redir *new_redir(int type, char *file)
+{
+	t_redir *node;
+	node = malloc(sizeof(t_redir));
+	if (!node)
+		return (NULL);
+	node->file = file;
+	node->type = type;
+	return (node);
+}
+
+void add_redir(t_cmd *cmd, t_redir *new)
+{
+	if (!cmd->redir)
+	{
+		cmd->redir = new;
+		return ;
+	}
+	while (cmd->redir->next)
+		cmd->redir = cmd->redir->next;
+	cmd->redir = new;
+}
+
+void add_arg(t_cmd *cmd, char *arg)
+{
+	
 }
