1c15bd8b9533239db4f6f9129bd11c62ae061711
Robinson Mukena Zevo <romukena@f2r7s16.clusters.42paris.fr>
2025-11-03 12:37:48 +0100

    finish utily functions for final function

--- parsing/main.c
+++ parsing/main.c
@@ -6,24 +6,44 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:01:15 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/01 17:38:25 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/03 12:10:52 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-int	main(void)
-{
-	t_node	*head;
-	// t_cmd	*final_head;
-	int		i;
+// int	main(void)
+// {
+// 	t_node	*head;
+// 	// t_cmd	*final_head;
+// 	int		i;
 
-	// char *str = "cat /etc/passwd | grep \"/bin/bash\" | cut -d: -f1 | sort | uniq | tr 'a-z' 'A-Z'";
-	// lexer(str, &head);
-	// handle_expands(&head);
-	// print_list(&head);
-	// int val = numb_of_commands(&head);
-	// printf("\nnumber is %d\n", val);
-	vivi();
+// 	// char *str = "cat /etc/passwd | grep \"/bin/bash\" | cut -d: -f1 | sort | uniq | tr 'a-z' 'A-Z'";
+// 	// lexer(str, &head);
+// 	// handle_expands(&head);
+// 	// print_list(&head);
+// 	// int val = numb_of_commands(&head);
+// 	// printf("\nnumber is %d\n", val);
+// 	vivi();
+// 	return (0);
+// }
+
+int main(void)
+{
+	t_cmd *cmd = new_cmd();
+
+	add_arg(cmd, "ls");
+	add_arg(cmd, "-l");
+	add_arg(cmd, "/home");
+
+	printf("Arguments ajoutés :\n");
+	for (int i = 0; cmd->argv && cmd->argv[i]; i++)
+		printf("[%s]\n", cmd->argv[i]);
+
+	// libération simple
+	for (int i = 0; cmd->argv && cmd->argv[i]; i++)
+		free(cmd->argv[i]);
+	free(cmd->argv);
+	free(cmd);
 	return (0);
 }
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,32 +6,13 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:41:37 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/01 18:11:02 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/03 12:37:11 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-int	numb_of_commands(t_node **head)
-{
-	int		i;
-	t_node	*tmp;
-
-	i = 0;
-	tmp = *head;
-	if (!head || !(*head))
-		return (i) ;
-	while (tmp)
-	{
-		if (tmp->type == PIPE)
-			return (i);
-		tmp = tmp->next;
-		i++;
-	}
-	return (i);
-}
-
-t_cmd *new_cmd(void)
+t_cmd	*new_cmd(void)
 {
 	t_cmd	*node;
 
@@ -45,9 +26,10 @@ t_cmd *new_cmd(void)
 	return (node);
 }
 
-t_redir *new_redir(int type, char *file)
+t_redir	*new_redir(int type, char *file)
 {
-	t_redir *node;
+	t_redir	*node;
+
 	node = malloc(sizeof(t_redir));
 	if (!node)
 		return (NULL);
@@ -57,10 +39,10 @@ t_redir *new_redir(int type, char *file)
 	return (node);
 }
 
-void add_redir(t_cmd *cmd, t_redir *new)
+void	add_redir(t_cmd *cmd, t_redir *new)
 {
-	t_redir *tmp;
-	
+	t_redir	*tmp;
+
 	tmp = cmd->redir;
 	if (!cmd->redir)
 	{
@@ -72,15 +54,38 @@ void add_redir(t_cmd *cmd, t_redir *new)
 	tmp->next = new;
 }
 
-void add_arg(t_cmd *cmd, char *arg)
+int	len_tab(char **tab)
 {
 	int	i;
+
 	i = 0;
-	if (!cmd->argv)
+	if (!tab)
+		return (0);
+	while (tab[i])
+		i++;
+	return (i);
+}
+
+void	add_arg(t_cmd *cmd, char *arg)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	**tab;
+
+	i = 0;
+	j = 0;
+	len = len_tab(cmd->argv);
+	tab = malloc(sizeof(char *) * (len + 2));
+	while (cmd->argv && cmd->argv[i])
 	{
-		cmd->argv = malloc(sizeof(char * ) * 1);
-		cmd->argv[0] = ft_strdup(arg);
-		return ;
+		tab[i] = ft_strdup(cmd->argv[i]);
+		i++;
 	}
-	return ;
+	tab[i] = ft_strdup(arg);
+	tab[i + 1] = NULL;
+	while (cmd->argv && cmd->argv[j])
+		free(cmd->argv[j++]);
+	free(cmd->argv);
+	cmd->argv = tab;
 }
