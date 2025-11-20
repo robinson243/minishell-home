30dcd94ecb729ea1ad00bd730f89cfb61e540706
Yamadou Dembele <ydembele@f2r6s6.clusters.42paris.fr>
2025-11-20 14:09:01 +0100

    leak

--- builtin/ft_export.c
+++ builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: ydembele <ydembele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 17:45:27 by ydembele          #+#    #+#             */
-/*   Updated: 2025/11/18 17:17:35 by ydembele         ###   ########.fr       */
+/*   Updated: 2025/11/20 13:53:32 by ydembele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,9 +87,9 @@ char	**do_export(char *arg, char **env, t_exec *exec)
 	{
 		exec->exit_code = 1;
 		perror("Malloc");
-		return (env);
+		return (free(normalized), env);
 	}
-	return (new_env);
+	return (free(normalized), new_env);
 }
 
 char	**ft_export(char **cmd, char **env, t_exec *exec)
