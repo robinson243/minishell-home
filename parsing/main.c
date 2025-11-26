3d4212a193b98e1cfb5903f4095944796fffcf22
Robinson Mukena Zevo <romukena@f1r7s4.clusters.42paris.fr>
2025-11-26 15:35:38 +0100

    make a good main

--- parsing/main.c
+++ parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:01:15 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/26 14:58:07 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/26 15:35:16 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,49 +122,4 @@ char	*my_gnl_stdin(void)
 	}
 	line[len] = '\0';
 	return (line);
-}
-
-int	main(int ac, char **av, char **envp)
-{
-	char	*line;
-	t_cmd	*cmd;
-	t_node	*node;
-	char	**env;
-	int		prv_code;
-
-	prv_code = 0;
-	env = ft_strdupdup(envp);
-	node = NULL;
-	cmd = NULL;
-	using_history();
-	(void)ac;
-	(void)av;
-	signals();
-	g_signal = 0;
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			line = readline("minishell > ");
-		else
-			line = my_gnl_stdin();
-		if (!line)
-			break ;
-		if (empty_line(line))
-		{
-			free(line);
-			continue ;
-		}
-		lexer(line, &node, env);
-		handle_expands(&node, env, prv_code);
-		cmd = parser(&node, &prv_code);
-		add_history(line);
-		free(line);
-		prv_code = exec(cmd, &env, node, prv_code);
-		clear_nodes(&node);
-		free_cmd_list_no_files(cmd);
-		g_signal = 0;
-	}
-	free_all(env);
-	clear_history();
-	return (prv_code);
 }
