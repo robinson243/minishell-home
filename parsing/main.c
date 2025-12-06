7e831dd512c3a24ab1dfeeda018bc8c22733853d
Yamadou Dembele <ydembele@f1br5s6.clusters.42paris.fr>
2025-12-06 19:52:40 +0100

    main

--- parsing/main.c
+++ parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: ydembele <ydembele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:01:15 by romukena          #+#    #+#             */
-/*   Updated: 2025/12/06 18:18:47 by ydembele         ###   ########.fr       */
+/*   Updated: 2025/12/06 19:52:28 by ydembele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	signals(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-/*int	main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
 	char	*line;
 	t_cmd	*cmd;
@@ -61,79 +61,79 @@ void	signals(void)
 	}
 	(free_all(env), clear_history());
 	return (prv_code);
-}*/
-
-char	*my_gnl_stdin(void)
-{
-	char	buffer[4096];
-	char	*line;
-	size_t	len;
-
-	if (!fgets(buffer, sizeof(buffer), stdin))
-		return (NULL);
-	len = 0;
-	while (buffer[len] && buffer[len] != '\n')
-		len++;
-	line = malloc(len + 2);
-	if (!line)
-		return (NULL);
-	for (size_t i = 0; i < len; i++)
-		line[i] = buffer[i];
-	if (buffer[len] == '\n')
-	{
-		line[len] = '\n';
-		len++;
-	}
-	line[len] = '\0';
-	return (line);
 }
 
-int	main(int ac, char **av, char **envp)
-{
-	char	*line;
-	t_node	*node;
-	char	**env;
-	int		prv_code;
+// char	*my_gnl_stdin(void)
+// {
+// 	char	buffer[4096];
+// 	char	*line;
+// 	size_t	len;
 
-	(void)ac;
-	(void)av;
+// 	if (!fgets(buffer, sizeof(buffer), stdin))
+// 		return (NULL);
+// 	len = 0;
+// 	while (buffer[len] && buffer[len] != '\n')
+// 		len++;
+// 	line = malloc(len + 2);
+// 	if (!line)
+// 		return (NULL);
+// 	for (size_t i = 0; i < len; i++)
+// 		line[i] = buffer[i];
+// 	if (buffer[len] == '\n')
+// 	{
+// 		line[len] = '\n';
+// 		len++;
+// 	}
+// 	line[len] = '\0';
+// 	return (line);
+// }
 
-	prv_code = 0;
-	node = NULL;
-	env = ft_strdupdup(envp);
-	signals();
-	using_history();
-	g_signal = 0;
+// int	main(int ac, char **av, char **envp)
+// {
+// 	char	*line;
+// 	t_node	*node;
+// 	char	**env;
+// 	int		prv_code;
 
-	while (1)
-	{
-		if (isatty(fileno(stdin)))
-		{
-			line = readline("minishell > ");
-			if (!line)
-				break ;
-		}
-		else
-		{
-			char *tmp = my_gnl_stdin();
-			if (!tmp)
-				break ;
-			line = ft_strtrim(tmp, "\n");
-			free(tmp);
-		}
-		if (empty_line(line))
-		{
-			free(line);
-			continue ;
-		}
-		add_history(line);
-		prv_code = process_command(line, &node, &env, prv_code);
-	}
+// 	(void)ac;
+// 	(void)av;
 
-	free_all(env);
-	clear_history();
-	return (prv_code);
-}
+// 	prv_code = 0;
+// 	node = NULL;
+// 	env = ft_strdupdup(envp);
+// 	signals();
+// 	using_history();
+// 	g_signal = 0;
+
+// 	while (1)
+// 	{
+// 		if (isatty(fileno(stdin)))
+// 		{
+// 			line = readline("minishell > ");
+// 			if (!line)
+// 				break ;
+// 		}
+// 		else
+// 		{
+// 			char *tmp = my_gnl_stdin();
+// 			if (!tmp)
+// 				break ;
+// 			line = ft_strtrim(tmp, "\n");
+// 			free(tmp);
+// 		}
+// 		if (empty_line(line))
+// 		{
+// 			free(line);
+// 			continue ;
+// 		}
+// 		add_history(line);
+// 		prv_code = process_command(line, &node, &env, prv_code);
+// 	}
+
+// 	free_all(env);
+// 	clear_history();
+// 	return (prv_code);
+// }
 
 
 
