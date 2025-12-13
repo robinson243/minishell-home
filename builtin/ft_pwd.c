4566d3aa3d45073cf3d6071916f0866deb0b1836
Robinson Mukena Zevo <romukena@f1r7s11.clusters.42paris.fr>
2025-12-13 15:09:20 +0100

    modify 1024

--- builtin/ft_pwd.c
+++ builtin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:56:28 by ydembele          #+#    #+#             */
-/*   Updated: 2025/12/10 11:55:07 by romukena         ###   ########.fr       */
+/*   Updated: 2025/12/13 15:07:52 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 
 int	ft_pwd(void)
 {
-	char	path[1024];
+	char	path[4096];
 
-	if (getcwd(path, 1024))
+	if (getcwd(path, 4096))
 	{
 		printf("%s\n", path);
 		return (0);
